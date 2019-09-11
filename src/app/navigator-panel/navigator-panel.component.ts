53902417af76b3d6e82c0428a4897f6222211518
Pamellino <pamellino@gmail.com>
2019-09-11 14:29:37 +0200

    3 files modified, 3 added

---
+++ src/app/navigator-panel/navigator-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigator-panel',
+  templateUrl: './navigator-panel.component.html',
+  styleUrls: ['./navigator-panel.component.css']
+})
+export class NavigatorPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
